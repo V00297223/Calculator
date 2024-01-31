ecda9ffee79fef117a8a14a389f1b371692664c8
V00297223 <157659814+V00297223@users.noreply.github.com>
2024-01-31 03:53:36 +0000

    controller added

---
+++ TestAPI/calculator.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class calculator : ControllerBase
+    {
+        [HttpGet]
+        public decimal Add(decimal num1, decimal num2)
+        {  
+            decimal result = num1 + num2;
+            return result;
+        }
+        
+        [HttpGet]
+        public decimal Subtract(decimal num1, decimal num2)
+        {  
+            decimal result = num1 - num2;
+            return result;
+        }
+        [HttpGet]
+        public decimal Multiply(decimal num1, decimal num2)
+        {  
+            decimal result = num1 * num2;
+            return result;
+        }
+        [HttpGet]
+        public decimal Divide(decimal num1, decimal num2)
+        {  
+            decimal result = num1 / num2;
+            return result;
+        }
+        [HttpGet]
+        public decimal Modulo(decimal num1, decimal num2)
+        {  
+            decimal result = num1 % num2;
+            return result;
+        }
+    }
+}
